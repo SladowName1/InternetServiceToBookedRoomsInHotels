faf678b746b2321c20efa186e778d5c6d7da05b4
SladowName1 <sladowname@mail.ru>
2022-04-17 14:29:59 +0300

    added modified on home page

--- src/components/home_page/BookingSearch.js
+++ src/components/home_page/BookingSearch.js
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import BackGroundImageForSearchContainer from '../../img/background_search.jpg'
+import axios from "axios";
+import EndPoint from "../const/EndPoint";
+import Select from 'react-select'
 
 const BookingSearch = () => {
+    const [cities, setCities] = useState(null);
+    const [startDate, setStartDate] = useState('');
+
+    useEffect(async () => {
+        const date = new Date();
+        let start = date.getFullYear() + '-' + pad2(date.getMonth() + 1) + '-' + pad2(date.getDate());
+        document.getElementById("start_date_input").setAttribute('min', start);
+        document.getElementById("end_date_input").setAttribute('min', start);
+        if (!cities) {
+            const resCities = await axios.get(`${EndPoint}api/hotel/cities`);
+            setCities(resCities.data.cities);
+        } else {
+            console.log(startDate);
+            cities.map(city => {
+                console.log(city);
+            })
+        }
+    });
+
+    const pad2 = (number) => {
+        return (number < 10 ? '0' : '') +number;
+    }
+
     return(
             <div className='booking_search_container'>
                 <div>
-                    Where you are going?
+                    Куда вы хотите поехать?
                 </div>
                 <div className='booking_search_container_element'>
                     <div className='booking_search_input'>
-                        <input className='input_element_where' placeholder='e.g Minsk'/>
+                            {cities?.length ?
+                                <div>
+                                <input className='input_element_at' placeholder='например Минск' type="text" name="city" list="cityname"/>
+                                    <datalist id="cityname" className='input_element_at'>
+                                        {cities.map((id, city) =>
+                                            <option key={city} value={id}/>)}
+                                    </datalist>
+                                </div>
+                            : <select>
+                                    <option>Минск</option>
+                                </select>}
                     </div>
                     <div className='booking_search_input'>
-                        <input className='input_element_at' placeholder='Check in'/>
+                        <input id='start_date_input' type='date' className='input_element_at' placeholder='Вьезд'/>
                     </div>
                     <div className='booking_search_input'>
-                        <input className='input_element_to' placeholder='Check out'/>
+                        <input id='end_date_input' type='date' className='input_element_to' placeholder='Выезд'/>
                     </div>
                     <div className='booking_search_input'>
-                        <input className='input_element_who' placeholder='How much people?'/>
+                        <input type='number' className='input_element_who' placeholder='Сколько людей'/>
                     </div>
                     <div className='booking_search_input'>
-                        <button className='button_search_hotels'>Search</button>
+                        <button className='button_search_hotels'>Найти</button>
                     </div>
                 </div>
             </div>
--- src/components/home_page/HotelsOnHomePage.js
+++ src/components/home_page/HotelsOnHomePage.js
@@ -1,44 +1,84 @@
-import React from 'react';
-import Image from '../../img/img.png'
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import EndPoint from "../const/EndPoint";
+import {Image} from 'cloudinary-react';
 
 const HotelsOnHomePage = () => {
+    const [offset, setOffset] = useState(0);
+    const [hotels, setHotels] = useState(null);
+    const [hotelsLenght, setHotelsLenght] = useState(0);
+    const [allHotels, setAllHotels] = useState(null)
+
+    useEffect( () => {
+        const interval = setInterval(async () => {
+            if (!allHotels && !hotelsLenght) {
+                const hotelsLength = await axios.get(`${EndPoint}api/hotel/`);
+                setHotelsLenght(hotelsLength.data.hotels.length);
+                setAllHotels(hotelsLength.data.hotels);
+                setHotels(hotelsLength.data.hotels.slice(offset, 5));
+                setOffset(5);
+            } else {
+                if (offset + 5 <= hotelsLenght) {
+                    setHotels(allHotels.slice(offset, offset+5));
+                    setOffset(offset + 5);
+                } else {
+                    setHotels(allHotels.slice(0, 5));
+                    setOffset(5);
+                }
+            }
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [allHotels, hotelsLenght, offset])
+
     return (
         <div className='home_image_container'>
             <div className='home_image_title'>
-                Some hotels in this service
+                Некоторые отели из нашего сервиса
             </div>
-            <div className='home_images_container'>
-                <div>
-                    <img src={Image} alt="Loading"/>
-                    <div className='hotel_image_title'>
-                        First Hotel
-                    </div>
-                </div>
-                <div>
-                    <img src={Image} alt="Loading"/>
-                    <div className='hotel_image_title'>
-                        Second Hotel
-                    </div>
-                </div>
-                <div>
-                    <img src={Image} alt="Loading"/>
-                    <div className='hotel_image_title'>
-                        Third Hotel
-                    </div>
-                </div>
-                <div>
-                    <img src={Image} alt="Loading"/>
-                    <div className='hotel_image_title'>
-                        Fourth Hotel
-                    </div>
-                </div>
-                <div>
-                    <img src={Image} alt="Loading"/>
-                    <div className='hotel_image_title'>
-                        Fifth Hotel
-                    </div>
-                </div>
-            </div>
+            {hotels ?
+                <div className='home_images_container'>
+                    {hotels.map(hotel =>
+                        <div key={hotel.id}>
+                           <Image
+                           cloudName = "dz3dswxup"
+                           publicId = 'v1650200281/test_dngcip.jpg'/>
+                            <div className='hotel_image_title'>
+                                {hotel.name}
+                            </div>
+                        </div>)}
+                </div> : <div>Loading</div>}
+            {/*<div className='home_images_container'>*/}
+            {/*    <div>*/}
+            {/*        <img src={Image} alt="Loading"/>*/}
+            {/*        <div className='hotel_image_title'>*/}
+            {/*            First Hotel*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={Image} alt="Loading"/>*/}
+            {/*        <div className='hotel_image_title'>*/}
+            {/*            Second Hotel*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={Image} alt="Loading"/>*/}
+            {/*        <div className='hotel_image_title'>*/}
+            {/*            Third Hotel*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={Image} alt="Loading"/>*/}
+            {/*        <div className='hotel_image_title'>*/}
+            {/*            Fourth Hotel*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div>*/}
+            {/*        <img src={Image} alt="Loading"/>*/}
+            {/*        <div className='hotel_image_title'>*/}
+            {/*            Fifth Hotel*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     )
 }
