c63a47337da30b28319e131c828b537d6ab9beb0
SladowName1 <sladowname@mail.ru>
2022-05-25 12:15:25 +0300

    added toast

--- src/components/modal/Login.js
+++ src/components/modal/Login.js
@@ -3,6 +3,7 @@ import React, {useContext, useEffect, useState} from "react";
 import "../../styles/login.css";
 import EndPoint from '../const/EndPoint';
 import {Context} from "../../index";
+import {toast} from "react-hot-toast";
 
 const Login = ({ active, setActive }) => {
   const [login, setLogin] = useState('');
@@ -11,29 +12,44 @@ const Login = ({ active, setActive }) => {
 
   const {user} = useContext(Context);
 
-  const submitLogin = async () => {
-      try{
-          const res = await axios.post(`${EndPoint}api/user/token?username=${login}&password=${password}`);
-          if(res.data.access_token) {
-              localStorage.setItem('token', res.data.access_token);
-              const config = {
-                  headers: { Authorization: `Bearer ${res.data.access_token}` }
-              };
+    const submitLogin = async () => {
+        try{
+            const checkEmail = document.getElementById('email_login')?.validity.valid;
+            const checkPassword = document.getElementById('password_login')?.validity.valid;
+            if(checkEmail && checkPassword) {
+                const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+                if(regex.test(login)) {
+                    const res = await axios.post(`${EndPoint}api/user/token?username=${login}&password=${password}`);
+                    if(res.data.access_token) {
+                        localStorage.setItem('token', res.data.access_token);
+                        const config = {
+                            headers: { Authorization: `Bearer ${res.data.access_token}` }
+                        };
 
-              const getUser = await axios.get(`${EndPoint}api/user/email?email=${login}`,config)
-              user.setUser(getUser.data.user);
+                        const getUser = await axios.get(`${EndPoint}api/user/email?email=${login}`,config)
+                        user.setUser(getUser.data.user);
 
-              setActive(false);
-              setError('');
-              setLogin('');
-              setPassword('');
-          }
-      } catch (err) {
-       if(err.response) {
-           setError(err.response.data.message);
-       }
-      }
-  }
+                        const getUserInfo = await axios.get(`${EndPoint}api/user/userInfo?id=${getUser.data.user.id}`, config);
+                        user.setUserInformation(getUserInfo.data.userInfo);
+
+                        setActive(false);
+                        setError('');
+                        setLogin('');
+                        setPassword('');
+                    }
+                } else {
+                    toast.error('введите валидный email');
+                }
+            } else {
+                toast.error('Заполните все поля');
+            }
+
+        } catch (err) {
+            if(err.response) {
+                toast.error(err.response.data.message);
+            }
+        }
+    }
 
   const cancel = () => {
       setActive(false);
@@ -42,21 +58,21 @@ const Login = ({ active, setActive }) => {
       setPassword('');
   }
 
-  return (
-    <div className="login-container">
-        {error ? <div>{error}</div> : null}
-      <div className="login-input-container login-email-container">
-        <input className="login-input" placeholder="Email" value={login} onChange={(e) => {setLogin(e.target.value)}}/>
-      </div>
-      <div className="login-input-container">
-        <input type="password" className="login-input" placeholder="Пароль" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
-      </div>
-      <div className="login-button-container">
-          <button onClick={() => submitLogin()}>Авторизация</button>
-          <button onClick={() => cancel()}>Отмена</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="login-container">
+            {error ? <div>{error}</div> : null}
+            <div className="login-input-container login-email-container">
+                <input className="login-input" id='email_login' placeholder="Email" value={login} onChange={(e) => {setLogin(e.target.value)}} required/>
+            </div>
+            <div className="login-input-container">
+                <input type="password" className="login-input" id='password_login' placeholder="Пароль" value={password} onChange={(e) => {setPassword(e.target.value)}} required/>
+            </div>
+            <div className="login-button-container">
+                <button onClick={() => submitLogin()}>Авторизация</button>
+                <button onClick={() => cancel()}>Отмена</button>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
--- src/components/modal/Registration.js
+++ src/components/modal/Registration.js
@@ -12,74 +12,88 @@ const Registration = ({ active, setActive }) => {
     const {user} = useContext(Context);
 
     const registration = async () => {
-        if (password === repeatPassword) {
-            try {
-                const data = {
-                    Email: login,
-                    Password: password
-                }
-                const res = await axios.post(`${EndPoint}api/user/registration`, data);
-                if(res.data.access_token) {
-                    localStorage.setItem('token', res.data.access_token);
-                    user.setUser(data);
+        const checkEmail = document.getElementById('email_registration')?.validity.valid;
+        const checkPassword = document.getElementById('password_registration')?.validity.valid;
+        const checkSecondPassword = document.getElementById('second_password_registration')?.validity.valid;
+        if(checkEmail && checkPassword && checkSecondPassword) {
+            if (password === repeatPassword) {
+                try {
+                    const data = {
+                        Email: login,
+                        Password: password
+                    }
+                    const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+                    if(regex.test(login)) {
+                        const res = await axios.post(`${EndPoint}api/user/registration`, data);
+                        if(res.data.access_token) {
+                            localStorage.setItem('token', res.data.access_token);
+                            user.setUser(data);
 
-                    const config = {
-                        headers: { Authorization: `Bearer ${res.data.access_token}` }
-                    };
+                            const config = {
+                                headers: { Authorization: `Bearer ${res.data.access_token}` }
+                            };
 
-                    const getUser = await axios.get(`${EndPoint}api/user/getByEmail?email=${login}`,config)
-                    console.log(getUser);
-                    user.setUser(getUser.data.user);
+                            const getUser = await axios.get(`${EndPoint}api/user/email?email=${login}`,config)
+                            console.log(getUser);
+                            user.setUser(getUser.data.user);
+                            const getUserInfo = await axios.get(`${EndPoint}api/user/userInfo?id=${getUser.data.user.id}`, config);
+                            user.setUserInformation(getUserInfo.data.userInfo);
 
-                    setActive(false);
-                    setError('');
-                    setLogin('');
-                    setPassword('');
-                    setRepeatPassword('');
-                }
-            } catch (err) {
-                if (err.response) {
-                    toast.error(err.response.data.message)
+                            setActive(false);
+                            setError('');
+                            setLogin('');
+                            setPassword('');
+                            setRepeatPassword('');
+                        }
+                    } else {
+                        toast.error('введите валидный email');
+                    }
+                } catch (err) {
+                    if (err.response) {
+                        toast.error(err.response.data.message)
+                    }
                 }
             }
-        }
-        else
+            else
             {
                 toast.error('Пароли не совпадают')
             }
+        } else {
+            toast.error('Заполните все поля')
         }
+    }
 
-        const logout = () => {
-            setActive(false);
-            setError('');
-            setLogin('');
-            setPassword('');
-            setRepeatPassword('');
-        }
+    const logout = () => {
+        setActive(false);
+        setError('');
+        setLogin('');
+        setPassword('');
+        setRepeatPassword('');
+    }
 
-        return (
-            <div>
-                <div className="login-input-container login-email-container">
-                    <input className="login-input" placeholder="Email" value={login} onChange={(e) => {
-                        setLogin(e.target.value)
-                    }}/>
-                </div>
-                <div className="login-input-container">
-                    <input type="password" className="login-input" placeholder="Пароль" value={password} onChange={(e) => {
-                        setPassword(e.target.value)
-                    }}/>
-                </div>
-                <div className="login-input-container">
-                    <input type="password" className="login-input" placeholder="Повтор пароля" value={repeatPassword} onChange={(e) => {
-                        setRepeatPassword(e.target.value)
-                    }}/>
-                </div>
-                <div className="login-button-container">
-                    <button onClick={() => registration()}>Регистрация</button>
-                    <button onClick={() => logout()}>Отмена</button>
-                </div>
+    return (
+        <div>
+            <div className="login-input-container login-email-container">
+                <input className="login-input" id='email_registration' placeholder="Email" value={login} onChange={(e) => {
+                    setLogin(e.target.value)
+                }} required/>
             </div>
-        );
-    };
+            <div className="login-input-container">
+                <input type="password" className="login-input" id='password_registration' placeholder="Пароль" value={password} onChange={(e) => {
+                    setPassword(e.target.value)
+                }} required/>
+            </div>
+            <div className="login-input-container">
+                <input type="password" className="login-input" id='second_password_registration' placeholder="Повтор пароля" value={repeatPassword} onChange={(e) => {
+                    setRepeatPassword(e.target.value)
+                }} required/>
+            </div>
+            <div className="login-button-container">
+                <button onClick={() => registration()}>Регистрация</button>
+                <button onClick={() => logout()}>Отмена</button>
+            </div>
+        </div>
+    );
+};
 
 export default Registration;
